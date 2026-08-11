0b482db11fd9e89902aeec28ad3174e63bc8001a
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2026-08-11 13:05:41 -0400

    Update FFXIPH_DatabaseQueryWrapper.php

--- includes/helpers/Database Management/FFXIPH_DatabaseQueryWrapper.php
+++ includes/helpers/Database Management/FFXIPH_DatabaseQueryWrapper.php
@@ -72,10 +72,10 @@ class DatabaseQueryWrapper {
         $day1 = (($vanaDay * 2)  + 1 );
         $day2 = ( $vanaDay * 2 );
         if ( !array_key_exists( $day1 , $arr) || !array_key_exists( $day2 , $arr) ){
-            wfDebugLog( 'Weather', get_called_class() . ":vanaday1 " . $day1 . ":vanaday2 " . $day2 .":array " . json_decode($arr) );
+            wfDebugLog( 'Weather', get_called_class() . ":vanaday1 " . $day1 . ":vanaday2 " . $day2 .":array " . json_encode($arr) );
             return 0;
         }
-        wfDebugLog( 'Weather', get_called_class() . ":vanaday1 " . $day1 . ":vanaday2 " . $day2 .":array " . json_decode($arr) );
+        wfDebugLog( 'Weather', get_called_class() . ":vanaday1 " . $day1 . ":vanaday2 " . $day2 .":array " . json_encode($arr) );
         $hexweatherdata =  $arr[ $day1 ] . $arr[ $day2 ];
         return $hexweatherdata;
     }
