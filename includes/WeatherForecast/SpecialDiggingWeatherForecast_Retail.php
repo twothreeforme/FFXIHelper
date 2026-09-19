d475bdc649de9382ab4b5a19ad63a84fec15f60e
Hugin <bradturner43@gmail.com>
2026-09-18 19:32:41 -0500

    Fixed some JS that was leaking at the top of page, fixed a cookie issue.

--- includes/WeatherForecast/SpecialDiggingWeatherForecast_Retail.php
+++ includes/WeatherForecast/SpecialDiggingWeatherForecast_Retail.php
@@ -14,6 +14,7 @@ class SpecialDiggingWeatherForecast_Retail extends SpecialPage {
 
         $request = $this->getRequest();
 		$output = $this->getOutput();
+        VanaTime::addTimezoneSync( $output );
 		//$output->setPageTitle( $this->msg( 'diggingweatherforecast' ) );
         $this->setHeaders();
 
