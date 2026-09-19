ad94d1384ae0877fdcf0c80f8866e035b6777a13
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2026-09-18 21:00:44 -0400

    Update FFXIPH_DatabaseQueryWrapper.php

--- includes/helpers/Database Management/FFXIPH_DatabaseQueryWrapper.php
+++ includes/helpers/Database Management/FFXIPH_DatabaseQueryWrapper.php
@@ -348,7 +348,7 @@ class DatabaseQueryWrapper {
             else $temp = ParserHelper::zoneERA_forList($row->name);
 			if ( !isset($temp) || ExclusionsHelper::zoneIsTown($temp)) { continue; }
 
-            $dayCount = 45;
+            $dayCount = 60;
             //check if on the diggers special page
             //should only include the weather for the zones listed in ExclusionsHelper::$diggingRelevantZones
             if ( $forDiggersPage == true) {
